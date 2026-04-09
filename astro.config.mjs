7f7079a2b6af8bf6ef939d4a4d508cd89e90479e
Tom Empson <tom.empson@icloud.com>
2026-04-09 22:34:01 +0100

    Adds About Us and Inside V8 pages and improves SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import sanity from "@sanity/astro";
 
 export default defineConfig({
+  site: "https://isov8.com",
   integrations: [
     sanity({
       projectId: "7esgb0i4",
